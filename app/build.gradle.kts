d92136f6a18974dac08e06963703058f3c2ffab0
gaurav <premanshu.solanki@cellulant.io>
2025-09-03 17:22:31 +0530

    Main Commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,25 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id ("org.sonarqube") version "5.1.0.4882"
+}
+
+
+sonarqube {
+    properties {
+        property("sonar.projectName", "truvideo-image-sdk")
+        property("sonar.projectKey", "com.truvideo.sdk.image")
+        property("sonar.host.url", "http://localhost:9000")
+        property("sonar.sources", "src/main") // list of sources
+        property("sonar.language", "kotlin")
+        property ("sonar.java.binaries", "build")
+        property("sonar.scm.disabled", "true")
+        property("sonar.token", "squ_faba1a72b53efe4c267d0966b354584632e28af5")
+
+        //property("sonar.login", "admin")
+        // property("sonar.password", "Anurag@9589393555")
+        property("sonar.gradle.skipCompile", "true") // ✅ Added to avoid implicit compilation
+    }
 }
 
 android {
---
+++ app/src/main/java/com/example/applinkdemo/ui/theme/THinkingMachine.java
@@ -0,0 +1,11 @@
+package com.example.applinkdemo.ui.theme;
+
+public class THinkingMachine {
+
+    public static String Members = "Been Removed always";
+    public int divide(int num)
+    {
+        return num/0;
+    }
+
+}
